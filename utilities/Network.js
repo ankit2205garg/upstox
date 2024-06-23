ed8beef7f5355fbc1e36de551b912658d2618fea
Ankit Garg <citrus>
2024-06-23 12:02:13 +0530

    removed duplicte files

--- utilities/Network.js
+++
@@ -1,7 +0,0 @@
-import {GET} from './networkFetch';
-
-const Api_GET_LIST = 'https://35dee773a9ec441e9f38d5fc249406ce.api.mockbin.io/';
-
-export const Network = {
-  getPortfolioList: () => GET(`${Api_GET_LIST}`),
-}
--- utilities/networkFetch.js
+++
@@ -1,9 +0,0 @@
-export const GET = async (api) => {
-  try {
-    const res = await fetch(api);
-    const jsonRes = await res.json();
-    return jsonRes;
-  } catch (e) {
-    return e;
-  }
-};
