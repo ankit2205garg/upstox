08fd3f8c90e564c7dfc995efe5271015ca4f6edb
Ankit Garg <citrus>
2024-06-23 11:42:50 +0530

    tsx changes

---
+++ components/TotalHoldings.tsx
@@ -0,0 +1,48 @@
+import { View, Text, StyleSheet } from "react-native";
+import { getTotalCumulativePrices } from "../utilities/commonMethods";
+import {ListItemInterface} from '../utilities/commonInterface';
+interface listDataIn{
+  listData: ListItemInterface[];
+  
+}
+const TotalHoldings = ({ listData }:listDataIn) => {
+  const cumulativeDataObj = getTotalCumulativePrices(listData);
+  const Items = [
+    {
+      key: "Current Value",
+      amount: cumulativeDataObj?.totalCurrentvalue,
+    },
+    {
+      key: "Total Investment",
+      amount: cumulativeDataObj?.totalInvestmentvalue,
+    },
+    {
+      key: "Todays Profit Loss",
+      amount: cumulativeDataObj?.totalClosevalue - cumulativeDataObj?.totalCurrentvalue,
+    },
+    {
+      key: " Profit & Loss",
+      amount: cumulativeDataObj?.totalCurrentvalue - cumulativeDataObj?.totalInvestmentvalue,
+    },
+  ];
+  return (
+    <View style={{padding:5}}>
+      {Items.map((item) => {
+        return (
+          <View key={item.key} style={styles.summaryItemRow}>
+            <Text>{item?.key}</Text>
+            <Text>&#8377;{item?.amount?.toFixed(2)}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+export default TotalHoldings;
+
+const styles = StyleSheet.create({
+  summaryItemRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
---
+++ utilities/commonInterface.tsx
@@ -0,0 +1,7 @@
+export interface ListItemInterface{
+  "symbol": string;
+   "quantity": number;
+    "ltp": number;
+    "avgPrice": number;
+    "close": number;
+}
